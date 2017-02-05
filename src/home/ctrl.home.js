dc43ddc7d983a30ae4f061e820b35fd459ab6c07
Neel Bommisetty <neel250294@gmail.com>
2017-02-06 02:49:27 +0530

    Added Fetch cards

--- src/home/ctrl.home.js
+++ src/home/ctrl.home.js
@@ -1,5 +1,34 @@
 export default class HomeController {
-  constructor() {
+  constructor(homeService) {
     this.greeting = 'Hello Bob !';
+    this.homeService = homeService;
+    this.propertyName = 'name';
+    this.reverse = false;
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    this.homeService.getUserDetails(this.username)
+    .then((res) => {
+      if (this.users) {
+        this.users.push(res.data);
+      } else {
+        this.users = [res.data];
+      }
+      this.username = '';
+    }).catch((res) => {
+      console.error(res.data); //eslint-disable-line
+    });
+  }
+  onDelete(e, index) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.users = this.users.filter((user, i) => i !== index);
+  }
+  sortBy(e, propertyName) {
+    e.preventDefault();
+    this.reverse = (this.propertyName === propertyName) ? !this.reverse : false;
+    this.propertyName = propertyName;
   }
 }
+
+HomeController.$inject = ['homeService'];
---
+++ src/home/service.home.js
@@ -0,0 +1,12 @@
+export default class HomeService {
+  constructor($http) {
+    this.$http = $http;
+  }
+  getUserDetails(username) {
+    return this.$http.get(`https://api.github.com/users/${username}`);
+  }
+  static HomeServiceFactory($http) {
+    return new HomeService($http);
+  }
+}
+
